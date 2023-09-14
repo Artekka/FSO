cf2e51ed731287d2a2d401ef64d99cd93251a9f8
Artekka <arthur.ohumukini@gmail.com>
2023-09-13 20:04:57 -0700

    FSO P1 C - OMG I added an action to a button

--- part01/src/App.jsx
+++ part01/src/App.jsx
@@ -4,13 +4,25 @@ const App = () => {
 
   const [counter, setCounter] = useState(0);
 
+  /*
   setTimeout(
     () => setCounter(counter + 1),
     1000
   )
+  */
+  
+  const handleClick = () => {
+    setCounter(counter + 1)
+    console.log("Click!")
+  }
+  
+  console.log("Rendering...", counter)
 
   return (
-    <div>{counter}</div>
+    <div id="alwaysHaveAnEnclosingTagWhenReturningMoreThanOneElement">
+      <div>{counter}</div>
+      <button onClick={handleClick}>Click Me!</button>
+    </div>
   )
 }
 
