ac0a429485f8e9eaa7374005341f3541d4569f46
Artekka <arthur.ohumukini@gmail.com>
2023-09-13 19:57:51 -0700

    FSO P1 C - Stateful component - Learning state!

--- part01/src/App.jsx
+++ part01/src/App.jsx
@@ -1,59 +1,18 @@
-import Hello from "./ComponentPractice";
+import { useState } from 'react'
 
-function App(props) {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7
-      },
-      {
-        name: "State of a component",
-        exercises: 14
-      }
-    ]
-  }
+const App = () => {
 
-  const userInfo = {
-    name: "Jason",
-    age: 37
-  }
-  
+  const [counter, setCounter] = useState(0);
 
-  const Header = (props) => {
-    console.log(props);
-    return <h1>{course.name}</h1>;
-  }
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
-  const Content = (props) => {
-    console.log(props);
-    return;
-  }
-
-  const Total = (props) => {
-    console.log(props);
-    return <p>Total number of exercises: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-  }
-  
-  const { counter } = props
-  
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={course}/>
-      <Total parts={course} />
-      <Hello
-        name={userInfo.name}
-        age={userInfo.age}
-      />
-      {counter}
-    </div>
+    <div>{counter}</div>
   )
 }
+
 
 export default App
--- part01/src/main.jsx
+++ part01/src/main.jsx
@@ -1,8 +1,4 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Counter from './components/Counter';
-let counter = 1;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
